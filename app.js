5c7780c8487e5b26dc387df8ff749a69b0518917
Peter Castle <vagnerpetel2020@gmail.com>
2021-06-22 11:41:42 -0300

    changes

--- app.js
+++ app.js
@@ -59,24 +59,47 @@ document.addEventListener("DOMContentLoaded", (e) => {
   //randomy select a Tetromino and its first rotation
   let randomTetromino = Math.floor(Math.random() * theTretrominoes.length);
 
-  let current = theTretrominoes[randomTetromino][currentRotation];
+  let currentTetromino = theTretrominoes[randomTetromino][currentRotation];
   //draw the tetromino
   function draw() {
-    current.forEach((index) => {
+    currentTetromino.forEach((index) => {
       squares[currentPosition + index].classList.add("tetromino");
     });
   }
+
   //undraw the Tetromino
   function undraw() {
-    current.forEach((index) => {
+    currentTetromino.forEach((index) => {
       squares[currentPosition + index].classList.remove("tetromino");
     });
   }
 
-  document.getElementById("start-button").addEventListener("click", PP);
+  document.getElementById("start-button").addEventListener("click", KK);
 
-  function PP() {
-    setInterval(draw(), 1000);
-    currentPosition = 5;
+  //number of row until get to the bottom
+  // 1(5) 2(15) 3(25) 4(35) 5(45) 6(55) 7(65) 8(75) 9(85) 10(95) 11(105)
+  // 12(115) 13(125) 14(135) 15(145) 16(155) 17(165) 18(175)
+
+  function KK() {}
+  // function PP() {
+  //   let myVarDraw = setInterval(() => {
+  //     if (currentPosition <= 175) {
+  //       undraw();
+  //       currentPosition = currentPosition + width;
+  //       draw();
+  //     } else {
+  //       clearInterval(myVarDraw);
+  //     }
+  //   }, 1000);
+  // }
+
+  // make the tetromino move down every second
+
+  let timerId = setInterval(moveDown, 1000);
+
+  function moveDown() {
+    undraw();
+    currentPosition += width;
+    draw();
   }
 });
